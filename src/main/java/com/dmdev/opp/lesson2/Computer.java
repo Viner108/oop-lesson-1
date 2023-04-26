ca4ee817e9b4f8a296f23c85564609b1218bd252
Viner108 <andrukovichl@mail.ru>
2023-04-26 19:27:18 +0300

    video 3 commit

--- src/main/java/com/dmdev/opp/lesson2/Computer.java
+++ src/main/java/com/dmdev/opp/lesson2/Computer.java
@@ -4,6 +4,17 @@ public class Computer {
     int ssd=500;
     int ram;
 
+    Computer(){
+        System.out.println("Я был создан");
+    }
+    Computer(int newSsd ){
+        ssd =newSsd;
+    }
+    Computer(int ssd, int newRam){
+        this.ssd=ssd;
+        ram=newRam;
+    }
+
     void load(){
         System.out.println("Я звгрузился");
     }
--- src/main/java/com/dmdev/opp/lesson2/ComputerRunner.java
+++ src/main/java/com/dmdev/opp/lesson2/ComputerRunner.java
@@ -4,8 +4,12 @@ public class ComputerRunner {
     public static void main(String[] args) {
         int value = 5;
         Computer computer = new Computer();
-        computer.load();
-        System.out.println("Ram = " +computer.ram);
-        System.out.println("Ssd = " +computer.ssd);
+        computer.printState();
+
+        Computer computer2 = new Computer(1050);
+        computer2.printState();
+
+        Computer computer3 = new Computer(10000, 10000);
+        computer3.printState();
     }
 }
